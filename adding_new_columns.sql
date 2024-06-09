59f1d40b521f0c5ed4dc9e8177f993a33e605f68
Natalia Finashina <nmfinashina@gmail.com>
2024-06-09 17:25:10 +0500

    Delete adding_new_columns.sql

--- adding_new_columns.sql
+++
@@ -1,7 +0,0 @@
-alter table staging.user_order_log add column status varchar(15) not null default 'shipped';
-COMMENT ON column staging.user_order_log.status is 'Статус заказа';
-
-alter table mart.f_sales add column status varchar(15) not null default 'shipped';
-COMMENT ON column mart.f_sales.status is 'Статус заказа';
-
-    
